9e172519ac8ed883a2f2e49e620777d0f207efe5
Jaehan <jbae12@my.centennialcollege.ca>
2021-07-16 22:10:37 -0400

    Send Email

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -3,37 +3,40 @@ import Users from '../models/userModel'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { generateActiveToken } from '../config/generateToken'
+import sendMail from '../config/sendMail'
+import { validateEmail } from '../middleware/valid'
+
+const CLIENT_URL = `${process.env.BASE_URL}`
 
 const authCtrl = {
-	register: async (req: Request, res: Response) => {
-		try {
-			const { name, account, password } = req.body
+  register: async (req: Request, res: Response) => {
+    try {
+      const { name, account, password } = req.body
 
-			const user = await Users.findOne({ account })
-			if (user)
-				return res
-					.status(400)
-					.json({ msg: 'Email or phone number already exists.' })
+      const user = await Users.findOne({ account })
+      if (user)
+        return res
+          .status(400)
+          .json({ msg: 'Email or phone number already exists.' })
 
-			const passwordHash = await bcrypt.hash(password, 12)
-			const newUser = {
-				name,
-				account,
-				password: passwordHash,
-			}
+      const passwordHash = await bcrypt.hash(password, 12)
+      const newUser = {
+        name,
+        account,
+        password: passwordHash,
+      }
 
-			const active_token = generateActiveToken(newUser)
+      const active_token = generateActiveToken(newUser)
+      const url = `${CLIENT_URL}/active/${active_token}`
 
-			res.json({
-				status: 'OK',
-				msg: 'Register successfullt.',
-				data: newUser,
-				active_token,
-			})
-		} catch (err) {
-			return res.status(500).json({ msg: err.message })
-		}
-	},
+      if (validateEmail(account)) {
+        sendMail(account, url, 'Verify your email address.')
+        return res.json({ msg: 'Success! Please check your email.' })
+      }
+    } catch (err) {
+      return res.status(500).json({ msg: err.message })
+    }
+  },
 }
 
 export default authCtrl
