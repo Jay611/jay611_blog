7cdf0cf84583f5abc6d0c021abacbea8b3523273
Jaehan <jbae12@my.centennialcollege.ca>
2021-07-17 15:49:40 -0400

    Client setting

---
+++ client/src/components/global/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: 'calc(100vh - 70px)' }}
+    >
+      <h2 className="text-secondary">404 | NotFound</h2>
+    </div>
+  )
+}
+
+export default NotFound
