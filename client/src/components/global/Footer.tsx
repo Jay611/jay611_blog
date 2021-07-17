92a663b8520ee945d61389552912bfb6a909cc60
Jaehan <jbae12@my.centennialcollege.ca>
2021-07-17 17:05:41 -0400

    Header

---
+++ client/src/components/global/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6>Welcome my repository Jay611</h6>
+      <a
+        href="https://github.com/Jay611"
+        className="mb-2 d-block text-info"
+        target="_blank"
+        rel="noreferrer"
+      >
+        https://github.com/Jay611
+      </a>
+      <p>Copyright &copy; 2021</p>
+    </div>
+  )
+}
+
+export default Footer
